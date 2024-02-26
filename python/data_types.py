92706f31c081a29d28869415fed98d766fedeb6b
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-02-26 20:40:02 +0300

    more of python varaibles

---
+++ python/data_types.py
@@ -0,0 +1,15 @@
+# data types --> specify the type of data to be stored inside a variable
+'''
+    # String Data Type: It is used to store text or characters. 
+    str_data = "Hello World"
+    
+    print(type(str_data))   # <class 'str'>
+
+    # Integer Data Type: It is used to store integer values (whole numbers).
+    int_data = 10
+
+    print(type(int_data))   # <class 'int'>
+
+    # Float Data Type: It is used to store floating point numbers, which are numbers with a decimal point.
+    float_data = 3.14
+'''
--- python/variables.py
+++ python/variables.py
@@ -1,12 +1,42 @@
 # how to create variables
 # the assignment operator is used to assign 
 full_name = 'Eric Nzyoka'
+site_name = "google.com"
 print(full_name)
+print('\t'  + site_name)
+
+# assign multiple variables
+print('\n')
+a, b, c, d, e = "Mutua", 10, 2.1, 'Hey', 'PLP'
+print("Variable a: ", a)
+print("Variable b: ", b)
+print("Variable c: ", c)
+print("Variable d: ", d)
+print("Variable e: ", e)
+
+# you can also use one line of code to declare and assign values to multiple variables
+print('\n')
+a = VariableA = "This is variable A"
+b = VariableB = "This is variable B"
+print(a)
+print(VariableA)
+
+# example 2
+print('\n')
+site1 = site2 = 'Power Learn Project'
+print(site1)
+print(site2)
+
+
+# you can also use tuple or list
+print('\n')
 
 # how to change variables names
 # assign new value to full_name
 full_name = "Nancy Onyango"
+site_name = "nancyonyango.za"
 print(full_name)
+print('\t' + site_name)
 
 # types of variables
 
