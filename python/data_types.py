6f1ae13ac0089f5d9530bf50236d26dbf51d0a3b
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-02-26 21:08:51 +0300

    more of python data types

--- python/data_types.py
+++ python/data_types.py
@@ -12,4 +12,78 @@
 
     # Float Data Type: It is used to store floating point numbers, which are numbers with a decimal point.
     float_data = 3.14
+    
+    print(type(float_data)) # <class 'float'>
+    
+    # Sequence data type: It is used to store sequence numbers, which are numbers with a sequence like in lists, tuples, range.
+        # list_languages = ["English", "swahili", "Arab"]
+    
+    # Boolean data type: It is used to store boolean values, which are boolean values with a boolean value (true/ false).
+    
+   #Examples below:
+    
 '''
+str_data = "Hello World"
+print(type(str_data))   # <class 'str'>
+    
+int_data = 5
+print(type(int_data)) # <class 'int'>
+
+float_data = 3.142
+print(type(float_data)) # <class 'float'>
+
+languages = ["Python", "Java", "Swift", "JavaScript"]
+print(type(languages)) # <class 'list'>
+    # accessing one element of the list
+print(languages[0]) # Python
+
+# Accessing multiple elements using slicing
+print(languages[0:2]) # ['Python', 'Java']
+
+# Adding new item to the list
+languages.append("C++") 
+print(languages)
+
+# python tuple data type
+# A tuple is an ordered sequence of items same as a list. 
+# the ONLY difference is that tuples are immutable while lists are mutable.
+# Tuples once created cannot be modified
+# in python, the parentheses () is used to store items in a tuple
+print('\n')
+
+# example below
+product = ('Xbox', 499.99, 'Lisher', 346.72, 'PLP', 'none')
+print(product)
+print(type(product)) # <class 'tuple'>
+print(len(product)) # shows its length
+print(product[0]) # Xbox
+print(product[1]) # access single element in the tuple
+print(product[2]) # access another single element in the tuple
+
+print('\n')
+
+# python String data type
+name = 'Python'
+print(name)
+
+message = 'Python for beginners'
+print(message)
+
+string_var = "This is a string."
+print('String Length : ', len(string_var), '\n')
+
+# python Set data type
+student_id = {112, 113, 114, 115, 116, 117, 118, 119}
+print(student_id)
+print(type(student_id))
+
+# example 2
+fruits = {'apple', 'banana', 'cherry'}
+print(fruits)
+print(type(fruits))
+
+# python Dictionary data type
+# dictionary --> is an ordered collection of items 
+# 
+
+
