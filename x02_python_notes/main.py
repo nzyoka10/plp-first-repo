b111ceb8945c8116b41529da49bf00511a314c06
mnzyokaeric@gmail.com <mnzyokaeric@gmail.com>
2024-02-27 10:12:50 +0300

    files

---
+++ x02_python_notes/main.py
@@ -0,0 +1,7 @@
+'''
+    # Reading keyboard input
+    This tutorial shows how to read user's keyboard input. The code is written in Python and displaying it on the screen.
+'''
+
+name = input('Enter your name: ')
+print(name)
